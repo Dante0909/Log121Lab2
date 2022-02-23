fa3e860a493ccb412177e94a2e9c0c646c554bd2
Luca <luca.borsato.1@ens.etsmtl.ca>
2022-02-23 15:29:51 -0500

    Implémentation de Joueur et De

--- src/framework/joueur/Joueur.java
+++ src/framework/joueur/Joueur.java
@@ -5,10 +5,35 @@ package framework.joueur;
  */
 public class Joueur implements Comparable<Joueur> {
 
+    private String nom;
+    private int score;
+
+    public Joueur(String nom) {
+        this.nom = nom;
+        this.score = 0;
+    }
+
     @Override
-    public int compareTo(Joueur o) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int compareTo(Joueur autre) {
+        Integer ceScore = Integer.valueOf(this.getScore());
+        Integer autreScore = Integer.valueOf(autre.getScore());
+        return ceScore.compareTo(autreScore);
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 
 }
