c9e8a835eda013e08d2e8d526bbbd042acad0e74
Luca <luca.borsato.1@ens.etsmtl.ca>
2022-02-23 16:23:29 -0500

    Implémentation des collections et iterateurs

--- src/framework/de/DeIterateur.java
+++ src/framework/de/DeIterateur.java
@@ -1,22 +1,40 @@
 package framework.de;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * DeIterateur
  */
 public class DeIterateur implements Iterator<De> {
 
+    private De[] des;
+    private int indice;
+
+    public DeIterateur(CollectionDes des) {
+        this.des = des.toArray();
+        this.indice = -1; // car on doit appeller next() au début pour avoir le premier element
+    }
+
     @Override
     public boolean hasNext() {
-        // TODO Auto-generated method stub
-        return false;
+        return this.indice + 1 < this.des.length;
     }
 
     @Override
     public De next() {
-        // TODO Auto-generated method stub
-        return null;
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        this.indice++;
+        return this.current();
+    }
+
+    public De current() {
+        if (this.indice == -1) {
+            throw new NoSuchElementException();
+        }
+        return this.des[this.indice];
     }
 
 }
